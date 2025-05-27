cb0b341e6e933e37ac3fc102fae86ed446af3797
Theo Lafont <tlafont49@gmail.com>
2025-05-27 08:09:44 +0200

    Relations in models added to the project

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,3 +1,14 @@
 class Vehicle < ApplicationRecord
   belongs_to :user
+  has_many :rentals, dependent: :destroy
+
+  validates :brand, presence: true
+  validates :model, presence: true
+  validates :daily_price, presence: true, numericality: { greater_than: 0 }
+  validates :localization, presence: true
+  validates :number_of_places, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :category, presence: true
+  validates :fuel_type, presence: true
+  validates :kilometers, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :available, inclusion: { in: [true, false] }
 end
