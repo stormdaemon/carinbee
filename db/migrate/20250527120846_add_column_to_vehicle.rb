e57d0e90f9ac879e43a59c57779d6cebfd70c737
Theo Lafont <tlafont49@gmail.com>
2025-05-27 14:09:23 +0200

    Added links img to vehicles

---
+++ db/migrate/20250527120846_add_column_to_vehicle.rb
@@ -0,0 +1,5 @@
+class AddColumnToVehicle < ActiveRecord::Migration[7.1]
+  def change
+    add_column :vehicles, :image_url, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_27_083500) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_27_120846) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,6 +66,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_27_083500) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
     t.index ["user_id"], name: "index_vehicles_on_user_id"
   end
 
