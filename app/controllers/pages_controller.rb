8ccc6beecebc489d299752c09155850dcf2656a5
Theo Lafont <tlafont49@gmail.com>
2025-05-30 08:56:54 +0200

    group_by_on_home_page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,10 @@ class PagesController < ApplicationController
 
   def home
     @featured_vehicles = Vehicle.where(available: true).limit(6)
+    # groupby vehicle categories
+    @vehicles = Vehicle.all
+    @vehicles_group_by = @vehicles.group_by(&:category)
+    @vehicles_group_by = @vehicles_group_by.sort_by { |category, vehicles| category }
+    @vehicles_group_by = @vehicles_group_by.first(3)
   end
 end
